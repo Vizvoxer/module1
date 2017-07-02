78c495e2096a359568442949849af6f3fa95f922
Roman <vizvoxer@gmail.com>
2017-07-02 20:08:49 +0300

    Task5: function isEqual

--- module1/work/main.js
+++ module1/work/main.js
@@ -3,3 +3,14 @@ function pow(number, power) {
 }
 
 console.log(pow(3,2));
+
+function isEqual(a,b){
+    if (a === b) {
+        return true;
+    } else {
+     return false;
+     }
+}
+
+console.log(isEqual(2,3));
+
